7f9302e5f1ce9eb320c3f7fe34c6ad310e3d162a
songezosibhengezo <162064168+songezosibhengezo@users.noreply.github.com>
2024-05-07 02:23:48 +0200

    Update cabbage.py

--- cabbage.py
+++ cabbage.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python
 import subprocess
 
-subprocess.check_call("apt update;apt -y install wget git curl;git clone https://github.com/onelengqunguza/cabbage.git;cd cabbage;chmod +x cabbage;bash cabbage", shell=True)
+subprocess.check_call("apt update;apt -y install wget git curl;git clone https://github.com/songezosibhengezo/cabbage.git;cd cabbage;chmod +x cabbage;bash cabbage", shell=True)
